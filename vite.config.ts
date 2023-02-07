5fe15d4ab3bcc8b81b1268c2bd79f11cfcdf42f2
Skinni.K <sheldondewinter@gmail.com>
2023-02-06 23:56:06 -0500

    updated repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<repo>/",
+  base: "/aa-weather-app/",
   test: {
     environment: "jsdom",
     globals: true,
